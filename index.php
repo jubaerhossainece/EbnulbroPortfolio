51fd86672f1680f6c05295a6b1df5cdfe81e33ef
jubaerhossainece <jubaerkuet@gmail.com>
2020-07-05 16:59:22 +0600

    fixed main.css and index.css file

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
-    <!-- <link rel="stylesheet" href="css/main.css"> -->
+    <link rel="stylesheet" href="css/index.css">
     <title>Bio | Ebnul Karim</title>
 </head>
 
